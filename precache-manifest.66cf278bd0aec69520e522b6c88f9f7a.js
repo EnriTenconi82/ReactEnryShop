a881c7f6301ed344161a4a414751d4e0a0ceed1b
EnriTenconi82 <enricotenconi82@gmail.com>
2022-12-27 18:23:27 -0300

    Updates

---
+++ precache-manifest.66cf278bd0aec69520e522b6c88f9f7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8543cc096deec895373",
+    "url": "/ReactEnryShop/static/css/main.f6588105.chunk.css"
+  },
+  {
+    "revision": "f8543cc096deec895373",
+    "url": "/ReactEnryShop/static/js/main.f8543cc0.chunk.js"
+  },
+  {
+    "revision": "c9b44b48ec3ae4b88dff",
+    "url": "/ReactEnryShop/static/js/1.c9b44b48.chunk.js"
+  },
+  {
+    "revision": "e9966507b97e5c69904c",
+    "url": "/ReactEnryShop/static/css/2.369ddbae.chunk.css"
+  },
+  {
+    "revision": "e9966507b97e5c69904c",
+    "url": "/ReactEnryShop/static/js/2.e9966507.chunk.js"
+  },
+  {
+    "revision": "9c4c0bb669e0fc4b737a",
+    "url": "/ReactEnryShop/static/js/runtime~main.9c4c0bb6.js"
+  },
+  {
+    "revision": "368bff899a6fe4ac9d303943e994f8fb",
+    "url": "/ReactEnryShop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactEnryShop/precache-manifest.71201b6a6cdc59ad3ef47c9488ca6502.js"
+  "/ReactEnryShop/precache-manifest.66cf278bd0aec69520e522b6c88f9f7a.js"
 );
 
 workbox.clientsClaim();
